afbb9136116903758c3a31c78d070d2f09eae40b
Marvin Kweyu <mkweyu1@gmail.com>
2022-08-30 06:44:56 +0300

    chore: add bug tag to henry class

--- henryClass.java
+++ henryClass.java
@@ -5,6 +5,8 @@
 // Write a method named henry that takes two integer arguments, i and j and returns the sum of the ith and jth perfect numbers. 
 // So for example, henry (1, 3) should return 502 because 6 is the 1st perfect number and 496 is the 3rd perfect number 
 // and 6 + 496 = 502.
+
+// BUG: I cannot find the factor of the ith or jth number
 public class henryClass {
     public static void main(String args[]) {
         System.out.println("The result is: " + henry(60, 3));
@@ -14,12 +16,6 @@ public class henryClass {
     public static int henry(int i, int j) {
         int result = 0;
 
-        // find factor of a number
-        // for (int item = 0; item < 60; item++) {
-        // if (60 % item == 0) {
-
-        // }
-        // }
         // find ith factor
         int count = 0;
         int factorOfI = 0;
@@ -28,9 +24,9 @@ public class henryClass {
             if (count == i) {
                 break;
             }
-            
+
             for (int item = 1; item < number; item++) {
-                
+
                 if (number % item == 0) {
                     int sumOfI = 0;
                     System.out.println("Factor  found: " + item);
