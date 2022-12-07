8e27a29e29886e4db93e399807b147fd2a1cce95
Marvin Kweyu <mkweyu1@gmail.com>
2022-12-07 14:45:25 +0300

    chore: add method to find power of integer

--- squareRoot.java
+++ squareRoot.java
@@ -8,6 +8,7 @@ public class squareRoot {
     public static void main(String[] args) {
         // * thought , find the number using a binary search
         System.out.println("the square root of 9 is: " + findRoot(25));
+        System.out.println("The power of: " + power_value(3, 2));
 
     }
 
@@ -51,14 +52,33 @@ public class squareRoot {
 
     public static double findRoot2(int number) {
         // Proceeding to find out square root of the number
-        double g1;
+        double guess;
         double squareRoot = number / 2;
         do {
-            g1 = squareRoot;
-            squareRoot = (g1 + (number / g1)) / 2; // Newton's method
-        } while ((g1 - squareRoot) != 0);
+            guess = squareRoot;
+            squareRoot = (guess + (number / guess)) / 2; // Newton's method
+        } while ((guess - squareRoot) != 0);
 
         return squareRoot;
+
+    }
+
+    public static int power_value(int base, int power) {
+        /*
+         * Find the power of an integer
+         */
+        int sum = 1;
+
+        for (int i = 0; i < power; i++) {
+            sum *= base;
+
+        }
+        return sum;
+
+    }
+
+    public static int multiple_arity(int... value) {
+        return 10;
 
     }
 }
